c01d05d8f5a5f03eb2e2a7af45dc6502267faba1
Jason Clay <jasonac1994@gmail.com>
2017-07-11 04:36:51 -0400

    walk directories to find paths

--- database.js
+++ database.js
@@ -1,74 +1,66 @@
 const fs = require('fs');
 const mime = require('mime-types');
-const readFilePromise = (path, encoding = 'utf8') => {
+const readdirPromise = pathRoot => {
     return new Promise((resolve, reject) => {
-        fs.readFile(path, encoding, (err, data) => {
+        fs.readdir(pathRoot, (err, dirPaths) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(data);
-            }
-        });
-    });
-};
-const readdirPromise = path => {
-    return new Promise((resolve, reject) => {
-        let paths = [];
-        fs.readdir(path, (err, dirPaths) => {
-            if (err) {
-                reject(err);
-            } else {
-                dirPaths.forEach(dirPath => {
-                    paths.push(`${path}/${dirPath}`);
+                let statPromises = [];
+                dirPaths.forEach(path => {
+                    statPromises.push(statPromise(pathRoot, path));
                 });
-                resolve(paths);
+                Promise.all(statPromises).then(dirPathsStats => {
+                    resolve(dirPathsStats);
+                }).catch(err => {
+                    reject(err);
+                });
             }
         });
     });
 };
-const statPromise = path => {
+const statPromise = (pathRoot, path) => {
     return new Promise((resolve, reject) => {
-        fs.stat(path, (err, pathStats) => {
+        var pathInfo = {}
+        fs.stat(`${pathRoot}/${path}`, (err, pathStats) => {
             if (err) {
                 reject(err);
             } else {
-                pathStats.path = path;
-                if (pathStats.isDirectory()) {
-                    pathStats.kind = 'directory';
-                } else if (pathStats.isFile()) {
-                    pathStats.kind = 'file';
-                    pathStats.type = mime.lookup(path);
+                if (pathStats.isFile()) {
+                    pathInfo = {
+                        'path': `${pathRoot}/${path}`,
+                        'kind': 'file',
+                        'type': mime.lookup(path)
+                    }
+                } else if (pathStats.isDirectory()) {
+                    pathInfo = {
+                        'path': `${pathRoot}/${path}`,
+                        'kind': 'directory'
+                    }
                 } else {
-                    reject('statPromise error');
+                    reject('file kind error');
                 }
-                resolve(pathStats);
+                resolve(pathInfo);
             }
         });
     });
 };
-const readdirRecursive = () => {
-    readdirPromise(dirToRead.pop()).then(paths => {
-        let fileStats = [];
-        paths.forEach(path => {
-            fileStats.push(statPromise(path));
-        });
-        return Promise.all(fileStats);
-    }).then(fileStats => {
-        fileStats.forEach(fileStat => {
-            if (fileStat.kind == 'directory') {
-                console.log(`'${fileStat.path}' is a '${fileStat.kind}'`);
-                dirToRead.push(fileStat.path);
+let directoriesLeft = [];
+const walkDir = (dir) => {
+    readdirPromise(dir).then(dirPaths => {
+        dirPaths.forEach(pathInfo => {
+            if (pathInfo.kind == 'directory') {
+                console.log(`Found directory '${pathInfo.path}'.`)
+                directoriesLeft.push(pathInfo.path);
             } else {
-                console.log(`'${fileStat.path}' is a '${fileStat.kind}' of type '${fileStat.type}'`);
+                console.log(`Found file '${pathInfo.path}' of type ${pathInfo.type}.`)
             }
         });
-        while (dirToRead.length != 0) {
-            readdirRecursive();
+        while (directoriesLeft.length > 0) {
+            walkDir(directoriesLeft.pop());
         }
     }).catch(err => {
         console.log(err);
     });
 };
-let dirToRead = [];
-dirToRead.push('database');
-readdirRecursive();
+walkDir('database');
